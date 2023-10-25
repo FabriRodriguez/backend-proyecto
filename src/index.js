696c63ec4bd12cdd81f9b5198c069b9d4d8e3841
Fabricio <fabiitricolor01@gmail.com>
2023-10-25 20:59:52 -0300

    arreglo final

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const port = 5000
 //definicion de endpoint get en ruta principal
 app.get('/', (request, response) => {
     response.send('hola mundo!')
-})
+});
+
+//listen = escuchar
+app.listen(puerto, ()=> {
+    console.log('escuchando el puerto ' + puerto)
+});
+
+//hola mundo
